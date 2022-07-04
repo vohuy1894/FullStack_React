03525a7364cebf95bddf2e9d28cac073b9fa498d
Huy Vo <vohuy1894@gmail.com>
2022-07-04 16:15:57 -0500

    test database

--- server/database/db.js
+++ server/database/db.js
@@ -6,12 +6,14 @@ const pool = new Pool({
     // host: process.env.PG_HOST,
     // port: process.env.PG_PORT,
     // database: process.env.PG_DATABASE,
-    user: "huyvo",
-    host: "localhost",
-    port: "5432",
-    database: "restaurantreact",
+    // user: "huyvo",
+    // host: "localhost",
+    // port: "5432",
+    // database: "restaurantreact",
+    connectionString: "postgres://bscmmkllrovnqr:897a97dc6e23f51c293c30b83511b5d4d4353044fb84b45ddf7f144e2840a1c7@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d9e7a0k4tkmmmn",
+    ssl: { rejectUnauthorized: false }
 });
-
+console.log(pool);
 // module.exports = pool;
 
 // const { Pool } = require("pg");
